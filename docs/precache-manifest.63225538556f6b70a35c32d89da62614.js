db7224e09780fb18441bf4a7fcf17239b655c21b
Pochi Jacob <pochijacob1993@gmail.com>
2020-10-10 11:14:49 -0300

    docs agregado

---
+++ docs/precache-manifest.63225538556f6b70a35c32d89da62614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e27b055913c19a542c052b05cb58d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2803fd87ea76f1b99000",
+    "url": "/static/css/main.91a1e4ff.chunk.css"
+  },
+  {
+    "revision": "80388b3f083f7dd673cf",
+    "url": "/static/js/2.6d56e17c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d56e17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2803fd87ea76f1b99000",
+    "url": "/static/js/main.6c686856.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
